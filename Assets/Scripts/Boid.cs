55b59b9e6a38a7e38a49e2f340b98694084cb263
DAVID-PC\David <davidazouz95@hotmail.com>
2017-03-26 23:12:17 +1100

    Trailer Scene: + Waypoint system from Standard Assets + Placeholder (?) Boat models + Water mat and shader from S.A. + PeerPlay Ripple Water that breaks Unity
    Scripts:
    + Buoyancy
    + Boid and Boid Manager

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System;
+//using System.Double;
+using Random = UnityEngine.Random;
+
+namespace Flocking
+{
+    [RequireComponent(typeof(Rigidbody))]
+    class Boid : MonoBehaviour
+    {
+        public new Transform transform;
+        public Rigidbody rb;
+        //public Vector3 velocity = Vector3.zero;
+        public Vector2 heading = Vector2.zero;
+        public float rotation = 0;
+        public float radius = 70;
+        public float maxVelocity = 50;
+
+        BoidManager boidManager;
+
+        //http://docs.oracle.com/javase/7/docs/api/java/util/Random.html#nextDouble()
+        /*public double NextDouble()
+        {
+            return (((long)next(26) << 27) + next(27))
+              / (double)(1L << 53);
+        } */
+
+        public void Start()
+        {
+            boidManager = BoidManager.Instance;
+            transform = GetComponent<Transform>();
+            rb = GetComponent<Rigidbody>();
+
+            transform.SetParent(BoidManager.Instance.transform);
+            float offset = 10.0f;
+            transform.position = new Vector3(Random.Range(-offset, offset), transform.parent.position.y, Random.Range(-offset, offset));
+
+            float x = Random.Range(0.0f, 1.0f) * 2 - 1;
+            float z = Random.Range(0.0f, 1.0f) * 2 - 1;
+            rb.velocity = new Vector3(x, 0, z) * maxVelocity;
+        }
+
+        /*public Boid(BoidManager _bm, Vector3 _position)
+        {
+            //GameObject go = new GameObject();
+            //go.AddComponent<Boid>();
+            //go.transform.SetParent(_bm.transform);
+        }*/
+
+        /*public void Spawn(BoidManager _bm, Vector3 _position)
+        {
+            transform.position = _position;
+
+            //random velocity to start with
+            //(float)NextDouble()
+
+            float x = Random.Range(0.0f, 1.0f) * 2 - 1;
+            float z = Random.Range(0.0f, 1.0f) * 2 - 1;
+            rb.velocity = new Vector3(x, 0, z) * maxVelocity;
+        }*/
+
+        public void FixedUpdate()
+        {
+            //get neighbours
+            Boid[] neighbours = boidManager.FindNeighbours(this);
+
+            //if we have at least 1 neighbour
+            if (neighbours.Length > 0)
+            {
+                //add all forces together and normalize
+                Vector3 force = Vector3.zero;
+                force += Separation(neighbours);
+                force += Alignment(neighbours);
+                force += Cohesion(neighbours);
+                force.Normalize();
+
+                //add force to velocity (as our forces are normalised, we multiply them by 100 so they have a greater influence on our heading)
+                rb.velocity += force * 100 * Time.deltaTime;
+                
+                //make us move at max speed all the time (not ideal but just for this example)
+                rb.velocity.Normalize();
+                rb.velocity *= maxVelocity;
+            }
+
+            //move position by velocity
+            transform.position += rb.velocity * Time.deltaTime;
+
+            //set our rotation //TODO: fix
+            heading = (rb.velocity == Vector3.zero ? new Vector3((float)-Math.Cos(rotation), (float)Math.Sin(rotation)) : rb.velocity);
+            heading.Normalize();
+            rotation = (float)Math.Atan2(heading.y, heading.x);
+        }
+
+        Vector3 Separation(Boid[] _boids)
+        {
+            Vector3 v = Vector3.zero;
+
+            //get average direction from neighbours to us
+            for (int i = 0; i < _boids.Length; ++i)
+                v += transform.position - _boids[i].transform.position;
+            v /= _boids.Length;
+
+            //subtract velocity to get force towards that direction
+            v -= rb.velocity;
+
+            //normalize and return
+            v.Normalize();
+            return v;
+        }
+
+        Vector3 Alignment(Boid[] _boids)
+        {
+            Vector3 v = Vector3.zero;
+            
+            //get average velocity of neighbours
+            for (int i = 0; i < _boids.Length; ++i)
+                v += _boids[i].rb.velocity;
+            v /= _boids.Length;
+            
+            //subtract velocity to get force towards that heading
+            v -= rb.velocity;
+            
+            //normalize and return
+            v.Normalize();
+            return v;
+        }
+
+        Vector3 Cohesion(Boid[] _boids)
+        {
+            Vector3 v = Vector3.zero;
+            
+            //get average position of neighbours
+            for (int i = 0; i < _boids.Length; ++i)
+                v += _boids[i].transform.position;
+            v /= _boids.Length;
+
+            //subtract position to get direction from us to average
+            v -= transform.position;
+            //subtract velocity to get force towards that direction
+            v -= rb.velocity;
+            
+            //normalize and return
+            v.Normalize();
+            return v;
+        }
+
+        /*public void Draw()
+        {
+            Rect rect = new Rect(position.x, position.y, boidManager.texture.width, boidManager.texture.height);
+            Vector3 origin = new Vector3(boidManager.texture.width * 0.5f, boidManager.texture.height * 0.5f);
+            //_sb.Draw(boidManager.texture, rect, null, Color.white, rotation, origin, SpriteEffects.None, 0);
+        }*/
+    }
+}
---
+++ Assets/Scripts/BoidManager.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+//using Random = UnityEngine.Random;
+
+namespace Flocking
+{
+    class BoidManager : MonoBehaviour
+    {
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // * Public Instance Variables { Class Variables}
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        //public Texture2D texture;
+        public Boid[] ships = new Boid[3];
+        //public Random rng = new Random();
+
+        List<Boid> boids = new List<Boid>();
+        float spawnFrequency = 0.05f;
+        float spawnCount = float.MaxValue;
+        Rect screenRect;
+
+        // Part of our game manager's Game Object that doesn't destroy 
+        // - therefore we don't want to create an instance
+        private static BoidManager _instance;
+        public static BoidManager Instance
+        {
+            get
+            {
+                if (_instance != null)
+                {
+                    return _instance;
+                }
+                // If we're null
+                BoidManager boidManager = FindObjectOfType<BoidManager>();
+                if (boidManager != null)
+                {
+                    _instance = boidManager;
+                }
+                return _instance;
+            }
+        }
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // ** Constructor
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public void Start()
+        {
+            //texture = _cm.Load<Texture2D>("Boid");
+            screenRect = new Rect(0, 0, Screen.width, Screen.height);
+            for (int i = 0; i <= ships.Length - 1; ++i)
+            {
+                Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+                Boid boid = (Boid)Instantiate(ships[i], position, Quaternion.identity);
+                boids.Add(boid);
+            }
+        }
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // * Class Function
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public void Update()
+        {
+            spawnCount += Time.deltaTime;
+            if (Input.GetMouseButtonDown(0) && spawnCount > spawnFrequency)
+            {
+                //Vector3 mousePos = Camera.current.ScreenToWorldPoint(new Vector3(100, 100, Camera.current.nearClipPlane));
+                //boids.Add(ships[Random.Range(0, ships.Length -1)]);
+                Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+                Boid boid = (Boid)Instantiate(ships[Random.Range(0, ships.Length - 1)], position, Quaternion.identity);
+                boids.Add(boid);
+                spawnCount = 0;
+            }
+
+            for (int i = 0; i < boids.Count; ++i)
+            {
+                boids[i].FixedUpdate();
+                if(!screenRect.Contains(new Vector2((int)boids[i].transform.position.x, (int)boids[i].transform.position.y)))
+                    boids.RemoveAt(i--);
+            }
+        }
+
+        /*public void Draw()
+        {
+            _sb.Begin();
+
+            for (int i = 0; i < boids.Count; ++i)
+                boids[i].Draw(_sb);
+
+            _sb.End();
+        }*/
+
+        public Boid[] FindNeighbours(Boid _of)
+        {
+            List<Boid> neighbours = new List<Boid>();
+
+            for (int i = 0; i < boids.Count; ++i)
+            {
+                if (boids[i] == _of)
+                    continue;
+
+                Vector2 vec = _of.transform.position - boids[i].transform.position;
+                float dist = vec.magnitude;
+
+                if (dist < _of.radius)
+                {
+                    neighbours.Add(boids[i]);
+                }
+            }
+
+            return neighbours.ToArray();
+        }
+
+        /*void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawLine(_of.transform.position, boids[i].transform.position);
+            Gizmos.DrawWireSphere(vec, _of.radius);
+        }*/
+    }
+}
